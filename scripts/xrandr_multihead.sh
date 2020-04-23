60335eb2d5cd0bce6a1063c5d76d8acb576a4d08
gshpychka <glibshpychka@gmail.com>
2020-04-24 00:42:57 +0300

    added xrandr setup

---
+++ scripts/xrandr_multihead.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0.8 --primary --mode 1920x1080 --pos 1080x0 --rotate right --output DVI-D-0 --off --output HDMI-0 --off --output DP-0 --off --output DP-1 --off --output DP-0.1.8 --mode 1920x1080 --pos 0x0 --rotate left
