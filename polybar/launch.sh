b89c06317927bfc17fdcdc37dccbd698bb8818c7
gshpychka <glibshpychka@gmail.com>
2020-11-25 13:27:20 +0200

    adjusted pomodoro settings

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,6 +6,10 @@ killall -q polybar
 # Wait until the processes have been shut fown
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# Clear output of pomobar module
+rm ~/pomobar.db
+echo "0" > ~/.pomobaroutput
+
 # Launch polybar using the confid location ~/.config/polybar/config
 polybar left &
 polybar right &
