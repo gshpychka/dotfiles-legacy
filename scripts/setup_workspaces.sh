35bbc18435438f86b96174b187689aafffbb94fa
gshpychka <glibshpychka@gmail.com>
2021-02-17 08:49:12 +0200

    changed firefox profile names

--- scripts/setup_workspaces.sh
+++ scripts/setup_workspaces.sh
@@ -1,7 +1,7 @@
 # /bin/sh
 
-firefox -P Work --class firefox_Work&
-firefox --new-instance -P default-release --class firefox_default&
+firefox -P work --class firefox_Work&
+firefox --new-instance -P default --class firefox_default&
 slack&
 telegram-desktop&
 exit
