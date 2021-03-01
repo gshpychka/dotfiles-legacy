488b4d49c832d9a5bfb7bd4084fb79ef47c5f84e
gshpychka <glibshpychka@gmail.com>
2021-03-01 13:12:32 +0200

    changed wallpaper

--- X/xinitrc.d/90-feh-wallpaper.sh
+++ X/xinitrc.d/90-feh-wallpaper.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-bg_filepath="$HOME/media/wallpapers/desktop_wallpaper.jpg"
+bg_filepath="$HOME/media/wallpapers/mars_wallpaper.png"
 
 if test -f "$bg_filepath"; then
     feh --no-fehbg --bg-fill --no-xinerama $bg_filepath
