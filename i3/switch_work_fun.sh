8f81144df72d637408ab58d04c670577868a6625
gshpychka <glibshpychka@gmail.com>
2021-04-01 12:03:43 +0300

    changed ws numbers

--- i3/switch_work_fun.sh
+++ i3/switch_work_fun.sh
@@ -5,7 +5,7 @@ RIGHT_WORK_WS_VIS=$(i3-msg -t get_workspaces | grep -P -o '(?<=\"name\":\"1\",\"
 if [ $RIGHT_WORK_WS_VIS = "true" ];
 then
     # switch to fun
-    i3-msg -t command "workspace 2; workspace 4"
+    i3-msg -t command "workspace 2; workspace 1"
 else
-    i3-msg -t command "workspace 1; workspace 3"
+    i3-msg -t command "workspace 4; workspace 3"
 fi
