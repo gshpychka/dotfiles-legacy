0ef3150678d533720d27760c4330fede65380492
gshpychka <glibshpychka@gmail.com>
2021-04-12 05:58:34 +0300

    added FF profile to setup

--- scripts/setup_workspaces.sh
+++ scripts/setup_workspaces.sh
@@ -2,6 +2,7 @@
 
 firefox -P work --class firefox_Work&
 firefox --new-instance -P default --class firefox_default&
+firefox --new-instance -P alpha --class firefox_alpha&
 slack&
 telegram-desktop&
 exit
