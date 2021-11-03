bf9a3eaa956d90a854c64330101841ed0c57ce33
Glib Shpychka <glibshpychka@gmail.com>
2021-11-03 09:21:06 +0200

    new monitors

--- lightdm/greeter_setup.sh
+++ lightdm/greeter_setup.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-xrandr --output DP-0.8 --primary --mode 1920x1080 --pos 1080x0 --rotate right --output DVI-D-0 --off --output HDMI-0 --off --output DP-0 --off --output DP-1 --off --output DP-0.1.8 --mode 1920x1080 --pos 0x0 --rotate left
+#xrandr --output DP-0.8 --primary --mode 1920x1080 --pos 1080x0 --rotate right --output DVI-D-0 --off --output HDMI-0 --off --output DP-0 --off --output DP-1 --off --output DP-0.1.8 --mode 1920x1080 --pos 0x0 --rotate left
+xrandr --output HDMI-0 --auto --pos 3840x0 --rotate right --output DP-0 --auto --pos 0x1225 --output DVI-D-0 --off --output DP-1 --off
