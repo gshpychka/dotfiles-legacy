3ead426184ab4c62bf9bdbb0aba4e493a05f03ad
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2022-06-17 14:52:54 +0300

    update LSP

--- nvim/lua/nvim-lspconfig.conf.lua
+++ nvim/lua/nvim-lspconfig.conf.lua
@@ -19,6 +19,7 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'i', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
@@ -26,32 +27,35 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.format { async = true }<CR>', opts)
 
   -- Set autocommands conditional on server_capabilities
   -- if client.server_capabilities.document_highlight then
-  --   vim.api.nvim_exec([[
-  --     hi LspReferenceRead ctermbg=LightGray ctermfg=none guibg='#7c6f64' guifg=none
-  --     hi LspReferenceText ctermbg=LightGray ctermfg=none guibg='#7c6f64' guifg=none
-  --     hi LspReferenceWrite ctermbg=LightGray ctermfg=none guibg='#7c6f64' guifg=none
-  --     augroup lsp_document_highlight
-  --       autocmd! * <buffer>
-  --       autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-  --       autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-  --     augroup END
-  --   ]], false)
+        -- hi LspReferenceRead guibg='#7c6f64' guifg=none
+        -- hi LspReferenceText guibg='#7c6f64' guifg=none
+        -- hi LspReferenceWrite guibg='#7c6f64' guifg=none
+    vim.api.nvim_exec([[
+        hi LspReferenceRead guibg='#665c54' guifg=none
+        hi LspReferenceText guibg='#665c54' guifg=none
+        hi LspReferenceWrite guibg='#665c54' guifg=none
+        augroup lsp_document_highlight
+        autocmd! * <buffer>
+        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+        augroup END
+    ]], false)
   -- end
-  -- vim.cmd [[autocmd CursorHold * lua vim.lsp.diagnostic.show_line_diagnostics()]]
-  -- vim.cmd [[autocmd CursorHoldI * silent! lua require('lspsaga.signaturehelp').signature_help()]]
-  -- vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  -- vim.lsp.diagnostic.on_publish_diagnostics, {
-  --     virtual_text = {
-  --         prefix = ""
-  --     },
-  --     underline = true,
-  --     signs = true,
-  -- }
-  -- )
+  vim.cmd [[autocmd CursorHold * lua vim.diagnostic.open_float(nil, {focus = false})]]
+  -- vim.cmd [[autocmd CursorHoldI * silent! lua vim.lsp.buf.signature_help()]]
+  vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+      virtual_text = {
+          prefix = ""
+      },
+      underline = true,
+      signs = true,
+  }
+  )
 
 end
 
