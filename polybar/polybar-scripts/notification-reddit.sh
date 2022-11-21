eecf6380e3dda17474aa9f9bcfa942338c07996f
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2022-11-21 15:32:58 +0200

    parametrize username

--- polybar/polybar-scripts/notification-reddit.sh
+++ polybar/polybar-scripts/notification-reddit.sh
@@ -3,7 +3,7 @@
 KEY=$(op read 'op://API/Reddit unread/password' --account my.1password.com)
 USERNAME=$(op read 'op://API/Reddit unread/username' --account my.1password.com)
 URL="https://www.reddit.com/message/unread/.json?feed=$KEY&user=$USERNAME"
-USERAGENT="polybar-scripts/notification-reddit:v1.0 u/Glebun"
+USERAGENT="polybar-scripts/notification-reddit:v1.0 u/$USERNAME"
 
 notifications=$(curl -sf --user-agent "$USERAGENT" "$URL" | jq '.["data"]["children"] | length')
 
