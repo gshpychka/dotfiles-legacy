8aa486644785193664f0f8452887fd9e916a3ffe
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2022-12-06 23:00:10 +0200

    suppress error messages

--- polybar/polybar-scripts/notification-reddit.sh
+++ polybar/polybar-scripts/notification-reddit.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-KEY=$(op read 'op://API/Reddit unread/password' --account my.1password.com)
-USERNAME=$(op read 'op://API/Reddit unread/username' --account my.1password.com)
+KEY=$(op read 'op://API/Reddit unread/password' --account my.1password.com 2>/dev/null)
+USERNAME=$(op read 'op://API/Reddit unread/username' --account my.1password.com 2>/dev/null)
 URL="https://www.reddit.com/message/unread/.json?feed=$KEY&user=$USERNAME"
 USERAGENT="polybar-scripts/notification-reddit:v1.0 u/$USERNAME"
 
-notifications=$(curl -sf --user-agent "$USERAGENT" "$URL" | jq '.["data"]["children"] | length')
+notifications=$(curl -sf --user-agent "$USERAGENT" "$URL" 2>/dev/null | jq '.["data"]["children"] | length' 2>/dev/null)
 
 if [ -n "$notifications" ] && [ "$notifications" -gt 0 ]; then
     echo "$notifications unread"
