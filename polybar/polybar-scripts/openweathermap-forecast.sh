feb1a349b42c881b03a3cc803d64ae0dbff08fd0
Glib Shpychka <23005347+gshpychka@users.noreply.github.com>
2022-12-06 23:03:41 +0200

    no output if key not retreived

--- polybar/polybar-scripts/openweathermap-forecast.sh
+++ polybar/polybar-scripts/openweathermap-forecast.sh
@@ -26,50 +26,52 @@ get_icon() {
     echo $icon
 }
 
-KEY=$(op read op://API/openweathermap/api-key --account my.1password.com)
+KEY=$(op read op://API/openweathermap/api-key --account my.1password.com 2>/dev/null)
 CITY="Kyiv"
 UNITS="metric"
 SYMBOL="°"
 
 API="https://api.openweathermap.org/data/2.5"
 
-if [ -n "$CITY" ]; then
-    if [ "$CITY" -eq "$CITY" ] 2>/dev/null; then
-        CITY_PARAM="id=$CITY"
+if [ -n "$KEY" ]; then
+    if [ -n "$CITY" ]; then
+        if [ "$CITY" -eq "$CITY" ] 2>/dev/null; then
+            CITY_PARAM="id=$CITY"
+        else
+            CITY_PARAM="q=$CITY"
+        fi
+
+        current=$(curl -sf "$API/weather?appid=$KEY&$CITY_PARAM&units=$UNITS")
+        forecast=$(curl -sf "$API/forecast?appid=$KEY&$CITY_PARAM&units=$UNITS&cnt=1")
     else
-        CITY_PARAM="q=$CITY"
+        location=$(curl -sf https://location.services.mozilla.com/v1/geolocate?key=geoclue)
+
+        if [ -n "$location" ]; then
+            location_lat="$(echo "$location" | jq '.location.lat')"
+            location_lon="$(echo "$location" | jq '.location.lng')"
+
+            current=$(curl -sf "$API/weather?appid=$KEY&lat=$location_lat&lon=$location_lon&units=$UNITS")
+            forecast=$(curl -sf "$API/forecast?appid=$KEY&lat=$location_lat&lon=$location_lon&units=$UNITS&cnt=1")
+        fi
     fi
 
-    current=$(curl -sf "$API/weather?appid=$KEY&$CITY_PARAM&units=$UNITS")
-    forecast=$(curl -sf "$API/forecast?appid=$KEY&$CITY_PARAM&units=$UNITS&cnt=1")
-else
-    location=$(curl -sf https://location.services.mozilla.com/v1/geolocate?key=geoclue)
+    if [ -n "$current" ] && [ -n "$forecast" ]; then
+        current_temp=$(echo "$current" | jq ".main.temp" | cut -d "." -f 1)
+        current_feels=$(echo "$current" | jq ".main.feels_like" | cut -d "." -f 1)
+        current_icon=$(echo "$current" | jq -r ".weather[0].icon")
 
-    if [ -n "$location" ]; then
-        location_lat="$(echo "$location" | jq '.location.lat')"
-        location_lon="$(echo "$location" | jq '.location.lng')"
+        forecast_temp=$(echo "$forecast" | jq ".list[].main.temp" | cut -d "." -f 1)
+        forecast_feels=$(echo "$forecast" | jq ".list[].main.feels_like" | cut -d "." -f 1)
+        forecast_icon=$(echo "$forecast" | jq -r ".list[].weather[0].icon")
 
-        current=$(curl -sf "$API/weather?appid=$KEY&lat=$location_lat&lon=$location_lon&units=$UNITS")
-        forecast=$(curl -sf "$API/forecast?appid=$KEY&lat=$location_lat&lon=$location_lon&units=$UNITS&cnt=1")
+        if [ "$current_temp" -gt "$forecast_temp" ]; then
+            trend=""
+        elif [ "$forecast_temp" -gt "$current_temp" ]; then
+            trend=""
+        else
+            trend=""
+        fi
+
+        echo "$(get_icon "$current_icon") $current_temp$SYMBOL ($current_feels$SYMBOL)  $trend  $(get_icon "$forecast_icon") $forecast_temp$SYMBOL ($forecast_feels$SYMBOL)"
     fi
-fi
-
-if [ -n "$current" ] && [ -n "$forecast" ]; then
-    current_temp=$(echo "$current" | jq ".main.temp" | cut -d "." -f 1)
-    current_feels=$(echo "$current" | jq ".main.feels_like" | cut -d "." -f 1)
-    current_icon=$(echo "$current" | jq -r ".weather[0].icon")
-
-    forecast_temp=$(echo "$forecast" | jq ".list[].main.temp" | cut -d "." -f 1)
-    forecast_feels=$(echo "$forecast" | jq ".list[].main.feels_like" | cut -d "." -f 1)
-    forecast_icon=$(echo "$forecast" | jq -r ".list[].weather[0].icon")
-
-    if [ "$current_temp" -gt "$forecast_temp" ]; then
-        trend=""
-    elif [ "$forecast_temp" -gt "$current_temp" ]; then
-        trend=""
-    else
-        trend=""
-    fi
-
-    echo "$(get_icon "$current_icon") $current_temp$SYMBOL ($current_feels$SYMBOL)  $trend  $(get_icon "$forecast_icon") $forecast_temp$SYMBOL ($forecast_feels$SYMBOL)"
 fi
